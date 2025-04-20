19bc658dfabac6f0e7a7e4cde91990b664f17bf6
vdovuhin <112893071+vdovuhin@users.noreply.github.com>
2025-04-20 11:46:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Строгий режим
-"use strict"
+'use strict'
 
 window.addEventListener('load', windowLoad);
 const html = document.documentElement;
@@ -7,12 +7,55 @@ const html = document.documentElement;
 function windowLoad() {
 	document.addEventListener('click', documentActions);
 	html.classList.add('loaded');
-}
+};
 function documentActions(e) {
 	const targetElement = e.target;
 
 	if (targetElement.closest('.icon-menu')) {
 		html.classList.toggle('menu-open');
-	}
+	};
 	targetElement.closest('.menu__link') && html.classList.contains('menu-open') ? html.classList.remove('menu-open') : null
-}
+};
+//========================================================================================================================================================
+const clientWrapper = document.querySelectorAll(".client__items");
+
+if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+	addAnimation();
+};
+
+function addAnimation() { 
+	clientWrapper.forEach((clientWrapper) => {
+    clientWrapper.setAttribute("data-animated", true);
+  });
+};
+//========================================================================================================================================================
+const blogSwiper = new Swiper(".blog__swiper", {
+  // Optional parameters  
+	spaceBetween: 24,	
+	loop: true,
+	breakpoints: {
+    // when window width is >= 320px
+    560: {
+      slidesPerView: 1.5,
+      
+    },
+    // when window width is >= 480px
+    992: {
+      slidesPerView: 2,
+      
+    },
+    // when window width is >= 640px
+    1000: {
+      slidesPerView: 3,
+     
+    }
+  },
+
+
+  // If we need pagination
+  pagination: {
+    el: ".pagination",       
+		clickable: true,
+	
+  },
+});
