ab6613729c9a6b97c3d1df89bee2f4b33ffc27f1
vdovuhin <112893071+vdovuhin@users.noreply.github.com>
2025-04-24 19:34:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,17 +17,37 @@ function documentActions(e) {
 	targetElement.closest('.menu__link') && html.classList.contains('menu-open') ? html.classList.remove('menu-open') : null
 };
 //========================================================================================================================================================
-const clientWrapper = document.querySelectorAll(".client__items");
+const clientSwiper = new Swiper(".client__wrapper", {
+  slidesPerView: 6,
+  spaceBetween: 60,
+  slidesOffsetAfter: 6,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+    },
 
-if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-	addAnimation();
-};
-
-function addAnimation() { 
-	clientWrapper.forEach((clientWrapper) => {
-    clientWrapper.setAttribute("data-animated", true);
-  });
-};
+    // when window width is >= 640px
+    760: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 60,
+    },
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+});
 //========================================================================================================================================================
 const blogSwiper = new Swiper(".blog__swiper", {
   // Optional parameters
